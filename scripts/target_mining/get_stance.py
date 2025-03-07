952d05dda3744ae1ad41a4effd635ac4a1eed39f
Ben Steel <bendavidsteel@gmail.com>
2025-03-07 15:24:41 -0500

    Added get stance script

---
+++ scripts/target_mining/get_stance.py
@@ -0,0 +1,29 @@
+import polars as pl
+
+import stancemining
+
+def main():
+
+    document_df = pl.read_parquet('./data/stance_targets/1day_doc_targets.parquet.zstd')
+
+    finetune_kwargs = {
+        'model_name': 'HuggingFaceTB/SmolLM2-135M-Instruct',
+        'add_system_message': True,
+        'save_model_path': '../stancemining/models/stancemining',
+        'prompting_method': 'stancemining',
+        'classification_method': 'generation',
+        'generation_method': 'list',
+        'batch_size': 2
+    }
+
+    model_kwargs = {
+        'device_map': 'auto',
+        'torch_dtype': 'auto'
+    }
+
+    miner = stancemining.StanceMining(finetune_kwargs=finetune_kwargs, model_kwargs=model_kwargs, verbose=True)
+    document_df, _ = miner.get_stance(document_df)
+    document_df.write_parquet('./data/stance_targets/1week_doc_targets_with_stance.parquet.zstd')
+
+if __name__ == "__main__":
+    main()
